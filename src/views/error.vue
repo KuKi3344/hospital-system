7f26515f3e6013699996a6b5a2523ca380836352
王璐 <568818216@qq.com>
2022-01-11 18:51:59 +0800

    无权限页面和404页面的添加

---
+++ src/views/error.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="bk" v-cloak>
+		<vue-particles color="#ffffff" :particleOpacity="0.7" :particlesNumber="70" shapeType="circle"
+			:particleSize="4" linesColor="#a0d7f9" :linesWidth="0" :lineLinked="false" :lineOpacity="0.7"
+			:linesDistance="80" :moveSpeed="4" :hoverEffect="false" hoverMode="grab" :clickEffect="false"
+			clickMode="push" class="lizi">
+		</vue-particles>
+		<div class ="nonetitle">
+			<div class="tip" style="font-size:100px;margin-top:120px;">ERROR</div>
+			<div class="tip2" style="margin-top:40px;margin-left:30px;line-height: 40px;">此页面不存在，小主可以回到<router-link to='/home'>首页</router-link></div>
+		
+		</div>
+		</div>
+</template>
+
+<script>
+	export default{
+		name:'error',
+	}
+</script>
+
+<style scoped="scoped">
+@import url("../assets/css/error.css");
+</style>
---
+++ src/views/none.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="bk">
+		<vue-particles color="#ffffff" :particleOpacity="0.7" :particlesNumber="70" shapeType="circle"
+			:particleSize="4" linesColor="#a0d7f9" :linesWidth="0" :lineLinked="false" :lineOpacity="0.7"
+			:linesDistance="80" :moveSpeed="4" :hoverEffect="false" hoverMode="grab" :clickEffect="false"
+			clickMode="push" class="lizi">
+		</vue-particles>
+		<div class ="nonetitle">
+			<div class="tip">404</div>
+			<div class="tip2">小主，您没有权限QWQ&emsp;请<router-link to='/'>登录</router-link></div>
+		
+		</div>
+		
+	
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'none'
+	}
+</script>
+
+<style scoped="scoped">
+@import url("../assets/css/error.css");
+</style>
