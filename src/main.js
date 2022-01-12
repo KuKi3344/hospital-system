252a60d6414789c89bf383c62832b3c753e02c6e
王璐 <568818216@qq.com>
2022-01-12 22:35:13 +0800

    将取设删cookie函数挂载到vue全局

--- src/main.js
+++ src/main.js
@@ -8,7 +8,11 @@ import VueParticles from 'vue-particles';
 // import store from './store';
 // import {initMenu} from './utils/menus';
 import axios from 'axios'
-
+import {
+	getCookieValue,
+	setCookieValue,
+	clearCookie
+}from './assets/js/cookie_utils'
 import{
 		getRequest,
 		putRequest,
@@ -28,25 +32,25 @@ Vue.prototype.putRequest = putRequest;
 Vue.prototype.getRequest = getRequest;
 Vue.prototype.deleteRequest = deleteRequest;
 
+
+Vue.prototype.getCookieValue = getCookieValue;
+Vue.prototype.setCookieValue =setCookieValue;
+Vue.prototype.clearCookie = clearCookie;
+
 //准备一个路由前置导航守卫
 router.beforeEach((to,from,next)=>{
 	//如果有token，不拦截
-	if(window.sessionStorage.getItem('token')){
+	if(getCookieValue("oars-token")){
 		// initMenu(router,store);
 		//通过前置路由守卫获取用户信息
 			//判断用户信息是否存在不存在就去获取
-		if(!window.sessionStorage.getItem('user')){
-			return getRequest('/user/info').then(resp=>{
-				if(resp){
-					window.sessionStorage.setItem('user',JSON.stringify(resp));
-				}
-				next();
-			})
-		}
+			if(!getCookieValue("user")){
+				next({ path: '/Login' })
+			}
 		next();
 	}else{
 		//如果没有token就拦截，如果去登录页面不拦截，如果去别的就给调到404页面
-		if(to.path=='/'||to.path=='/Login'||to.path=='/404'||to.path=='/Regist'||to.path=='/home'){
+		if(to.path=='/'||to.path=='/Login'||to.path=='/404'||to.path=='/Regist'){
 			next();
 		}
 		else{
