d0af9645665eb633405872e2f10e0665a4cdaa09
王璐 <568818216@qq.com>
2022-01-15 16:50:21 +0800

    管理员右侧主界面

--- src/components/adminview.vue
+++ src/components/adminview.vue
@@ -1,70 +1,39 @@
 <template>
-	<div>
-	<el-table v-loading="loading" :data="dataList" height="500" border
-		style="width:100%; border-radius:10px;" :header-cell-style="{background:'#eef1f4',color:'#606266'}">
-		<el-table-column fixed prop="registration.id" label="问诊单id" width="120" height="40">
-		</el-table-column>
-		<el-table-column prop="person.id" label="病人id" width="120" height="40">
-		</el-table-column>
-		<el-table-column prop="person.name" label="病人姓名" width="120" height="40">
-		</el-table-column>
-		<el-table-column prop="doctor.id" label="预约医生id" width="120" height="40">
-		</el-table-column>
-		<el-table-column prop="doctor.name" label="预约医生姓名" width="120" height="40">
-		</el-table-column>
-		<el-table-column prop="registration.createDate" label="挂号申请日期" width="120" height="40">
-		</el-table-column>
-		<el-table-column prop="registration.subDate" label="挂号预约日期" width="120" height="40">
-		</el-table-column>
-		<el-table-column prop="registration.handleDate" label="挂号处理日期" width="120" height="40">
-		</el-table-column>
-		<el-table-column prop="registration.registStatus" label="挂号状态" width="100" height="40">
-			<template slot-scope="scope">
-				<el-tag>{{scope.row.registration.registStatus}}</el-tag>
-			</template>
-		</el-table-column>
-		
-		<el-table-column prop="registration.enquiryStatus" label="问诊状态" width="100" height="40">
-			<template slot-scope="scope">
-				<el-tag>{{scope.row.registration.enquiryStatus}}</el-tag>
-			</template>
-		</el-table-column>
-		
-		
-		<el-table-column label="操作" width="150" height="40">
-	
-			<template slot-scope="scope">
-				<el-button type="primary" size="small" @click="updatePos(scope.row)">编辑</el-button>
-				 <el-button type="danger" size="small" @click="deletePos(scope.row.id)">删除</el-button>
-			</template>
-	
-		</el-table-column>
-	
-	</el-table>
+		<div>
+		 <el-tabs  v-model="activeName" @tab-click="handleClick">
+		    <el-tab-pane label="用户管理" name="first">
+				<masuser></masuser>
+			</el-tab-pane>
+		    <el-tab-pane label="问诊单汇总" name="second">
+				<maslist></maslist>
+			</el-tab-pane>
+		  </el-tabs>
+		</div>
 	</div>
 </template>
 
 <script>
+	import masuser from './admin/masuser'
+	import maslist from './admin/maslist'
 	export default{
-		name:"userview",
+		name:"adminview",
+		components:{
+			maslist,
+			masuser
+			
+		},
 		data(){
 			
 			return{
-				loading:true,
-				dataList:[],
+				activeName:'second'
 				
 			}
 		},
-		created(){
-			let id = this.getCookieValue("userid");
-			// console.log(this.filterList)
-			this.$axios.get('/api/pdr/query/all').then(resp=>{
-				this.dataList = resp.data;
-				this.loading = false;
-				console.log(resp.data)
-			})
-			
-		},
+		 methods: {
+		      handleClick(tab, event) {
+		        console.log(tab, event);
+		      }
+		    }
 	}
 </script>
 
